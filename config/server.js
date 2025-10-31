65843abed868f917c290e8095b3eeeaef26eccd2
Tomás Ignacio <33787632+tom-ignacio@users.noreply.github.com>
2025-10-31 10:59:52 -0400

    Ping every 14 minutes

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ import routes from "../app/routes/routes.js";
 import { connectToDatabase } from './db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -20,5 +21,10 @@ export const startServer = async () => {
     await connectToDatabase();
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
+        setInterval(() => {
+        fetch(process.env.URL)
+            .then(() => console.log('Self-ping sent'))
+            .catch(err => console.error('Ping failed:', err));
+        }, 14 * 60 * 1000);
     });
 };
